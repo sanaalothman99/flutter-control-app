cf6d52e1d029eca85ca2b8683cce4553d84f9026
sanaalothman99 <sanaalothmann@gmail.com>
2025-08-18 17:20:41 +0200

    fix:update gradle config for codemagic build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,42 +1,53 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.flutter_projects"
-    compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    // غيّري الـ namespace لمعرّفك
+    namespace = "com.sana.fluttercontrol"
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    // هذه القيم يوفّرها Flutter Gradle plugin
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_projects"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
-        targetSdk = 35
-        versionCode = 1
-        versionName ="1.0"
+        // نفس الـ namespace عادةً
+        applicationId = "com.sana.fluttercontrol"
+
+        // ✅ صيغة Kotlin DSL الصحيحة (بدلاً من minSdkVersion/targetSdkVersion)
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+
+        // يقرأان من gradle.properties (flutter.versionCode / flutter.versionName)
+        versionCode = project.property("flutter.versionCode").toString().toInt()
+        versionName = project.property("flutter.versionName").toString()
+
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
 }
 
 flutter {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0" apply false
+}
 allprojects {
     repositories {
         google()
